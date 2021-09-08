2739e7c8d51bff2b572b90bccf8fce24d7f3ae9d
hanifkf12 <bagiyo666@gmail.com>
2021-09-08 18:41:00 +0700

    change url2

--- util/payment_url.js
+++ util/payment_url.js
@@ -1,12 +1,12 @@
 exports.paymentUrl = {
-    BCA: `http://116.193.191.200/payment-service/api/v1/va/create`,
-    BNI: `http://116.193.191.200/payment-service/api/v1/va/create`,
-    BNI_SYARIAH: `http://116.193.191.200/payment-service/api/v1/va/create`,
-    MANDIRI: `http://116.193.191.200/payment-service/api/v1/va/create`,
-    PERMATA: `http://116.193.191.200/payment-service/api/v1/va/create`,
-    SAHABAT_SAMPOERNA: `http://116.193.191.200/payment-service/api/v1/va/create`,
-    BRI: `http://116.193.191.200/payment-service/api/v1/va/create`,
-    CIMB: `http://116.193.191.200/payment-service/api/v1/va/create`,
+    BCA: `https://pusanair-dev.xyz/payment-service/api/v1/va/create`,
+    BNI: `https://pusanair-dev.xyz/payment-service/api/v1/va/create`,
+    BNI_SYARIAH: `https://pusanair-dev.xyz/payment-service/api/v1/va/create`,
+    MANDIRI: `https://pusanair-dev.xyz/payment-service/api/v1/va/create`,
+    PERMATA: `https://pusanair-dev.xyz/payment-service/api/v1/va/create`,
+    SAHABAT_SAMPOERNA: `https://pusanair-dev.xyz/payment-service/api/v1/va/create`,
+    BRI: `https://pusanair-dev.xyz/payment-service/api/v1/va/create`,
+    CIMB: `https://pusanair-dev.xyz/payment-service/api/v1/va/create`,
     ALFAMART: ``,
     INDOMARET: ``
 }
